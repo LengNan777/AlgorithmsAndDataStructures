9496936260dc31bc08410542fd97446975394cb4
LengNan777 <L2452520160@gmail.com>
2022-06-11 16:06:00 -0500

    finish lab2

--- Labs/Program.cs
+++ Labs/Program.cs
@@ -1,53 +1,87 @@
 ﻿using System;
 
-Console.WriteLine("Please enter a number:");
-int enteredNumber = int.Parse(Console.ReadLine());
-Console.WriteLine("Please enter {0} words:",enteredNumber);
-
+int enteredNumber =0;
 string words = "";
-for(int i = 0; i < enteredNumber; i++)
-{
-    Console.WriteLine("Word {0}:",i+1);
-    string newWord = Console.ReadLine();
-    if (newWord.Any(Char.IsWhiteSpace))
-    {
-        Console.WriteLine("Please do not enter space. Enter the word again.");
-        i--;
-        continue;
-    }
-    words += newWord;
-}
-
-
+char enteredContent = ' ';
 int counter = 0;
 char enteredChar = '?';
 bool isChar = false;
+int percentage = 0;
 
-do {
-    Console.WriteLine("Please enter a character");
-    char enteredContent = char.Parse(Console.ReadLine());
+GetNumberInput();
+GetWordsInput();
+CountCharacters(words, enteredContent);
+GetCharacterOccurrencePercentage(words, counter);
+PrintResults(percentage, enteredChar, counter);
 
-    if (Char.IsLetter(enteredContent)) {
-        isChar = true;
-        enteredChar = enteredContent;
-        for (int i = 0; i < words.Length; i++)
+int GetNumberInput()
+{
+    Console.WriteLine("Please enter a number:");
+    enteredNumber = int.Parse(Console.ReadLine());
+    return enteredNumber;
+}
+
+void GetWordsInput()
+{
+    Console.WriteLine("Please enter {0} words:", enteredNumber);
+
+    for (int i = 0; i < enteredNumber; i++)
+    {
+        Console.WriteLine("Word {0}:", i + 1);
+        string newWord = Console.ReadLine();
+        if (newWord.Any(Char.IsWhiteSpace))
         {
-            if (words[i] == enteredContent)
+            Console.WriteLine("Please do not enter space. Enter the word again.");
+            i--;
+            continue;
+        }
+        words += newWord;
+    }
+}
+
+char getCharacterInput()
+{
+    Console.WriteLine("Please enter a character");
+    return char.Parse(Console.ReadLine());
+}
+
+int CountCharacters(string words, char charToCount)
+{
+    do
+    {
+        enteredContent = getCharacterInput();
+
+        if (Char.IsLetter(enteredContent))
+        {
+            isChar = true;
+            enteredChar = enteredContent;
+            for (int i = 0; i < words.Length; i++)
             {
-                counter++;
+                if (words[i] == enteredContent)
+                {
+                    counter++;
+                }
             }
         }
+    } while (!isChar);
+    return counter;
+}
+
+int GetCharacterOccurrencePercentage(string words, int counter)
+{
+    percentage = counter * 100 / words.Length;
+    return percentage;
+}
+
+void PrintResults(int percentage, char enteredChar, int counter)
+{
+    if (percentage > 25)
+    {
+        Console.WriteLine("The letter '{0}' appears {1} times in the array. This letter makes up more the 25% of the total number of characters,", enteredChar, counter);
     }
-} while (!isChar);
+    else
+    {
+        Console.WriteLine("The letter '{0}' appears {1} times in the array.", enteredChar, counter);
+    }
 
-int totalLength = words.Length;
-int percentage = counter * 100 / totalLength;
-if (percentage > 25)
-{
-    Console.WriteLine("The letter '{0}' appears {1} times in the array. This letter makes up more the 25% of the total number of characters,",enteredChar, counter);
 }
-else
-{
-    Console.WriteLine("The letter '{0}' appears {1} times in the array.",enteredChar,counter);
-}
-
